1e466cd1d9450122f8373d28eff5f88a8f6f7af3
Rahul Kaura <rahulkaura2004@gmail.com>
2019-07-29 18:26:27 -0700

    Create ZombieKillerClass.java

---
+++ ZombieKillerClass.java
@@ -0,0 +1,29 @@
+package ZombieKiller;
+
+public class ZombieKillerClass {
+
+	public static void main(String[] args) {
+		//Shotgun kills Close Range Zombies - 0
+		//Assault Rifle kills Mid Range Zombies - 1
+		//Sniper Rifle kills Long Range Zombies - 2
+		System.out.println("Welcome to My Zombie Killer Program!");
+		
+		String backpack[] = {"Shotgun", "Assault Rifle", "Sniper"};
+		String zombies[] = {"Close Range Zombies" , "Mid-Range Zombies", "Long-range Zombies"};
+		
+
+		System.out.println("Backpack Items!");
+		System.out.println(backpack[0]);
+		System.out.println(backpack[1]);
+		System.out.println(backpack[2]);
+		
+		System.out.println("These are the Zombies!");
+		System.out.println(zombies[0]);
+		System.out.println(zombies[1]);
+		System.out.println(zombies[2]);
+		
+		
+
+	}
+
+}
